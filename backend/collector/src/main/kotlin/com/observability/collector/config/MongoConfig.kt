e217111de987e198f4bd9d165170bb197e0031e8
Utkarsh bhola <utkarsh.bhola@gmail.com>
2025-12-07 16:24:36 +0530

    changed mongoconfig cause Mongo URI was hardcoded

--- backend/collector/src/main/kotlin/com/observability/collector/config/MongoConfig.kt
+++ backend/collector/src/main/kotlin/com/observability/collector/config/MongoConfig.kt
@@ -1,28 +1,34 @@
 package com.observability.collector.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.mongodb.MongoDatabaseFactory
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
-import org.springframework.context.annotation.Primary
-@Primary
+
 @Configuration
-class MongoConfig {
+class MongoConfig(
+    @Value("\${MONGO_URI_LOGS}") private val logsUri: String,
+    @Value("\${MONGO_URI_MAIN}") private val metaUri: String
+) {
+
+    @Primary
+    @Bean
+    fun metaDbFactory(): MongoDatabaseFactory =
+        SimpleMongoClientDatabaseFactory(metaUri)
+
+    @Primary
+    @Bean
+    fun metaMongoTemplate(): MongoTemplate =
+        MongoTemplate(metaDbFactory())
 
     @Bean
     fun logsDbFactory(): MongoDatabaseFactory =
-        SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/logsDB")
+        SimpleMongoClientDatabaseFactory(logsUri)
 
     @Bean
     fun logsMongoTemplate(): MongoTemplate =
         MongoTemplate(logsDbFactory())
-
-    @Bean
-    fun metaDbFactory(): MongoDatabaseFactory =
-        SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/metaDB")
-
-    @Bean
-    fun metaMongoTemplate(): MongoTemplate =
-        MongoTemplate(metaDbFactory())
 }
